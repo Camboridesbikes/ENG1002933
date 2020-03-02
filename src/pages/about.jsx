9a499c47ce585e16fb74ffb096c03fe8e4e200f4
Cameron Hattendorf <hattendorfcameron@gmail.com>
2020-03-02 11:21:17 -0800

    +vampi

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -34,13 +34,14 @@ const SecondPage = () => (
     <h1>Characters</h1>
     {dispChar}
     <div style={{marginLeft: "1.5rem", width: "45%"}}>
-      <h2>Naylie</h2>
+      <h2 id="">Naylie</h2>
       <p>She is known as a very bright, strong,athletic character who is extremely admired by loyal friends. They all just graduated high school but all share a bleak future in the poor Mexican village of Tres Camarones. 
 </p>
       <h2>Yolo</h2>
       <p>Yolo is short for Yoloxochitl.  She is a very beautiful girl who is from Tres Camarones.  Yolo has short hair that she cut for the summer.   Yolo is also very smart.  It is referenced many times in the book how she was a straight A student.  She played soccer and was good, but not as good as Nayeli. She is an avid reader.  She works at a bowling alley as a pin setter.  Yolo, like Nayeli, has a crush on Matt, the missionary.    </p>
       <h2>Vampi</h2>
-      <p></p>
+      <p>Veronica, known as Vampi, is the only goth girl in the state of Sinaloa. Her look is distinctive: black hair dyed even blacker, pale makeup, black lips and nails, and a long black skirt. Despite her appearance, Vampi’s experience with goth culture is limited, derived from Youtube where she worships bands such as The 69 Eyes and Type O Negative. No parent in Tres Camarones would allow their child to dress that way but Vampi’s grandma was old and too tired to tell her anything. Only the notorious girlfriends noticed that Vampi went goth after her parents died. Behind the front is a girl who still has a lot to learn about the world. An example of this is shown when Nayeli and Yolo had to help her use a tampon because her grandma never let her use one or when she didn’t know who the president of her country was. However, she has street smarts to an extent and this is shown when she translated what the cholos at the border told them about crossing, or when she sat on the old cowboy on the bus because she took advantage of the fact that she was a young teenage girl and the man was elderly. 
+</p>
       <h2>Tacho</h2>
       <p></p>
       <p>Tacho is the gay proprietor and cook of the village restaurant, La Mano Caída (The Fallen Hand), a frequent hangout for Nayeli and her girlfriends. Tacho and Nayeli are kindred spirits, both dreaming of a cosmopolitan life in a big city. They sometimes climb up on the roof of the restaurant to look at the sky, “making believe that clouds were the Manhattan skyline.</p>
